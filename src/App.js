cc71f0a2b09607dd81a5d57cb9426ad50656e885
Viknesh Vky <vikneshvky7@gmail.com>
2025-12-05 15:57:42 +0530

    Use REACT_APP_API_URL for backend base URL

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import "./App.css";
 
-const API_URL = "http://localhost:8080/api/items";
+const API_URL =
+  process.env.REACT_APP_API_URL || "http://localhost:8080/api/items";
 
 function initialForm() {
   return {
