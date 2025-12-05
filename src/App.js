f56357f664580410290b7c74fb2e53873678187a
Viknesh Vky <vikneshvky7@gmail.com>
2025-12-05 16:22:57 +0530

    Adjust API_URL config

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import "./App.css";
 
-const API_URL =
-  process.env.REACT_APP_API_URL || "http://localhost:8080/api/items";
+const API_URL =process.env.REACT_APP_API_URL || "http://localhost:8080/api/items";
 
 function initialForm() {
   return {
